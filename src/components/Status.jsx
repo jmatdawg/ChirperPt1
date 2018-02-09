eba81a74c2f513dfebe60a65971d9a32cf3c7ce2
jmatdawg <matthews_jakiera@yahoo.com>
2018-02-09 14:10:36 -0600

    still working

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -6,11 +6,13 @@ class Status extends Component {
         // console.log(props);
         return (
             <Fragment>
-                <input className = "form-control-lg center"
+                <input className = "form-control-lg w-75"
                     placeholder="What's Chirpin'?"
                     onChange = { (event) => {this.props.handleInput(event.target.value)}}
                     />
-                <button onClick= {this.props.onAdd}>SUBMIT</button>
+                <button 
+                    onClick= {this.props.onAdd}
+                    className="btn btn-warning">SUBMIT</button>
             </Fragment> 
         )
     }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,10 +5,17 @@ class User extends Component {
     render() {
         return (
            <Fragment>
-               <img alt= "" src= "https://i1.wp.com/ayibamagazine.com/wp-content/uploads/2016/11/blackgirlmagicfeauture.jpg?w=622" className="rounded-circle avatar"/>
-                <h2> I AM HER.</h2>
-                <h4 className="d-inline"> @BlkGirlMagic </h4><img className= "check" src="https://cdn3.iconfinder.com/data/icons/buttons/512/Icon_13-512.png" alt=""/>
-                <button className="pull-right">FOLLOW HER</button>
+
+                   <img alt= "" src= "https://i1.wp.com/ayibamagazine.com/wp-content/uploads/2016/11/blackgirlmagicfeauture.jpg?w=622" className="rounded-circle avatar"/>
+               
+                <div className="d-flex justify-content-between">
+                     <h2> I AM HER.</h2>
+                     <button className="btn btn-warning ">FOLLOW HER</button></div>
+                   
+                    <h4 className="d-inline"> @BlkGirlMagic </h4><img className= "check" src="https://cdn3.iconfinder.com/data/icons/buttons/512/Icon_13-512.png" alt=""/>
+                    
+                
+                
             </Fragment>
         )
     }
